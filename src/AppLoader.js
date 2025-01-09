2d0399089471cb87c490060eee125eb80d4c042d
wwwouaiebe <github.538f6722-9860-473d-9057-57f9bf96554c@guyette.be>
2025-01-09 22:28:13 +0100

    Build ongoing

--- src/AppLoader.js
+++ src/AppLoader.js
@@ -25,11 +25,10 @@ Changes:
 import OsmDataLoader from './OsmDataLoader.js';
 import GtfsTreeBuilder from './GtfsTreeBuilder.js';
 import OsmTreeBuilder from './OsmTreeBuilder.js';
-import RouteMasterComparator from './RouteMasterComparator.js';
 import theReport from './Report.js';
 import theExcludeList from './ExcludeList.js';
-import { theOsmTree, theGtfsTree } from './DataTree.js';
 import GtfsDataLoader from './GtfsDataLoader.js';
+import OsmGtfsComparator from './OsmGtfsComparator.js';
 
 /* ------------------------------------------------------------------------------------------------------------------------- */
 /**
@@ -45,100 +44,6 @@ class AppLoader {
 
 	constructor ( ) {
 		Object.freeze ( this );
-	}
-
-	/**
-     * Coming soon
-     */
-
-	#compareOsmGtfs ( ) {
-
-		// loop on osm route master
-		theOsmTree.routesMaster.forEach (
-			osmRouteMaster => {
-				let errorMessage = '';
-				let gtfsRouteMaster = null;
-
-				// Searching a GTFS route master with the same ref
-				let gtfsRoutesMaster =
-					theGtfsTree.routesMaster.filter ( element => osmRouteMaster.ref === element.ref );
-				switch ( gtfsRoutesMaster.length ) {
-				case 0 : // no gtfs route master found
-					errorMessage = 'The route ' + osmRouteMaster.ref + ' is not in the gtfs files.';
-					break;
-				case 1 : // one gtfs route master found
-					gtfsRouteMaster = gtfsRoutesMaster [ 0 ];
-					break;
-				default :
-
-					// more than one gtfs route master found
-					// Searching a GTFS route master with the same ref and same description
-					gtfsRoutesMaster = theGtfsTree.routesMaster.filter (
-						element => {
-							let returnValue =
-								osmRouteMaster.ref === element.ref
-								&&
-								0 === osmRouteMaster.description.toLowerCase ( ).localeCompare (
-									element.description.toLowerCase ( )
-								);
-							return returnValue;
-						}
-					);
-					if ( 1 === gtfsRoutesMaster.length ) {
-
-						// one gtfs route master found
-						gtfsRouteMaster = gtfsRoutesMaster [ 0 ];
-					}
-					else {
-						errorMessage = 'No GTFS route master found. ' +
-							'Be sure that the osm route master have the same description than the GTFS route master.';
-					}
-					break;
-				}
-
-				if ( gtfsRouteMaster ) {
-
-					// A GTFS route master exists. Adapting the GTFS route master
-					gtfsRouteMaster.osmRouteMaster = true;
-
-					// Start the comparison
-					new RouteMasterComparator ( ).compare ( osmRouteMaster, gtfsRouteMaster );
-				}
-				else {
-
-					// no GTFS route master found
-					theReport.add ( 'h2', errorMessage );
-				}
-			}
-		);
-	}
-
-	/**
-     * Coming soon
-     */
-
-	#searchMissingOsmRouteMaster ( ) {
-
-		theReport.add ( 'h1', 'Gtfs relations not found in the osm data' );
-
-		// loop on the GTFS routes master
-		theGtfsTree.routesMaster.forEach (
-			routeMaster => {
-				const excludedString = theExcludeList.getExcludeReason ( routeMaster.ref );
-				if ( excludedString ) {
-					theReport.add ( 'p', excludedString );
-				}
-				else if ( ! routeMaster.osmRouteMaster ) {
-					theReport.add ( 'p', 'gtfs route ref : ' + routeMaster.ref );
-					routeMaster.routes.forEach (
-						route => {
-							theReport.add ( 'p', route.name, null, route.shapePk );
-						}
-					);
-					theReport.addToDo ( routeMaster.routes.length );
-				}
-			}
-		);
 	}
 
 	/**
@@ -177,11 +82,12 @@ class AppLoader {
 		new GtfsTreeBuilder ( ).buildTree ( );
 
 		// compare existing osm route master with gtfs route
-		this.#compareOsmGtfs ( );
+		let osmGtfsComparator = new OsmGtfsComparator ( );
+		osmGtfsComparator.compare ( );
 
 		// Search Missing osm route master only if no osm ref given by user
 		if ( ! osmRef ) {
-			this.#searchMissingOsmRouteMaster ( );
+			osmGtfsComparator.searchMissingOsmRouteMaster ( );
 		}
 
 		// close...
